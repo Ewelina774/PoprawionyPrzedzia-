f629d944e91c89a19b453955274d2b4f710082f7
Ewelina774 <ewelina_hofman98@wp.pl>
2019-07-25 16:17:15 +0200

    Dodanie

---
+++ src/Numbers.java
@@ -0,0 +1,6 @@
+class Numbers {
+    public static void main(String[] args) {
+        double i = 0;
+        while (i <= 30) {
+            System.out.println("Liczba " + i / 10);
+            i++; } }}
---
+++ src/NumbersDoWhile.java
@@ -0,0 +1,6 @@
+public class NumbersDoWhile {
+    public static void main(String[] args) {
+        double i = 0;
+        do { System.out.print( i/10 + ", "); i++;
+        } while(i <= 29);
+        System.out.println(i/10); i++; }}
